5f9d165b34510ce8e680690960a2e1344117f815
linalouise <linalouise@linalouise.com>
2018-02-13 18:45:50 +0000

    software-properties-common

--- scripts/dependencies-ubuntu.sh
+++ scripts/dependencies-ubuntu.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update -qq
 sudo apt-get upgrade -y -qq
-sudo apt-get install -y -qq autoconf build-essential pkg-config libssl-dev libboost-all-dev
+sudo apt-get install -y -qq autoconf build-essential pkg-config libssl-dev libboost-all-dev software-properties-common
 sudo apt-get install -y -qq miniupnpc libminiupnpc-dev gettext
 #for gui
 sudo apt-get install -y -qq qtbase5-dev qttools5-dev-tools
