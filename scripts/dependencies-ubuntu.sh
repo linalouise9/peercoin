e5042da0b12f3914dcfccd04f766a4ff0d7ab050
linalouise <linalouise@linalouise.com>
2018-02-13 18:09:13 +0000

    gcc,g++7

--- scripts/dependencies-ubuntu.sh
+++ scripts/dependencies-ubuntu.sh
@@ -7,4 +7,10 @@ sudo apt-get install -y -qq miniupnpc libminiupnpc-dev gettext
 #for gui
 sudo apt-get install -y -qq qtbase5-dev qttools5-dev-tools
 sudo apt-get install -y -qq libdb++-dev
+#latest g++
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -qq
+sudo apt-get install gcc-7 g++-7 -yqq
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+g++ --version
 
